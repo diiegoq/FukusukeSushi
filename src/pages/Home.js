528a0934a012e2cef354c4cee40c16bb3f20a2d6
diego <diegoconchaaa@gmail.com>
2025-04-07 23:42:33 -0400

    detalles navbar + base homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import './Home.css';
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <Container className='home-container'>
+      <Row>
+        <Col xs={12} sm={2}>Column 1</Col>
+        <Col xs={12} sm={8}>Column 2</Col>
+        <Col xs={12} sm={2}>Column 3</Col>
+      </Row>
+    </Container>
+  );
 };
-  
+
 export default Home;
